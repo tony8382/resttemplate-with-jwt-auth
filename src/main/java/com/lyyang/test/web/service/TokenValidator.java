dbd0e17aafb5af796b4e3ffe7f21737cb8aac898
Li-Yu, Yang <tony830802@gmail.com>
2023-02-28 12:36:25 +0800

    check expire with 1 min buffer

--- src/main/java/com/lyyang/test/web/service/TokenValidator.java
+++ src/main/java/com/lyyang/test/web/service/TokenValidator.java
@@ -2,6 +2,7 @@ package com.lyyang.test.web.service;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.interfaces.DecodedJWT;
+import org.apache.commons.lang3.time.DateUtils;
 import org.springframework.stereotype.Service;
 
 import java.util.Date;
@@ -11,7 +12,8 @@ public class TokenValidator {
 
     public boolean isTokenValid(String token) throws Exception {
         DecodedJWT jwt = JWT.decode(token);
-        if (jwt.getExpiresAt().before(new Date())) {
+
+        if (jwt.getExpiresAt().before(DateUtils.addMinutes(new Date(), 1))) {
             return false;
         }
         return true;
